5a0e5e8cfa7bfa658684cd29390e5c04c3b2ef0a
FerRafapanther <fernandorafael@ciencias.unam.mx>
2025-03-20 15:54:13 -0600

    Update Main.py

--- teoria/relacionesDeClases/Main.py
+++ teoria/relacionesDeClases/Main.py
@@ -5,7 +5,7 @@ Created on Tue Feb 18 16:20:41 2025
 @author: Rocket
 """
 # Fecha: Marzo, 2025
-# @version: 1.2
+# @version: 1.3
 # @autor: FernandoRafaelLopezSolano
 
 from Menu import *
@@ -15,25 +15,17 @@ from Cuenta import *
 class Main:
     pass
 
-menu1= Menu("Bienvenido a banxico")
-menu1.darBienvenida()
-menu1.desplegarOpciones()
+cuenta1 = Cuenta(450, "debito", "Fernando")
+cliente1 = Cliente("Fernando", "Ciudad de México", 19, cuenta1)
 
-cuenta1= Cuenta(450, "debito", "Fernando")
-cliente1= Cliente("Fernando", "ciudad de mexico", 19, cuenta1)
 print("Detalles del cliente:")
-print(f"Nombre: {cliente1.nombre}")
-print(f"Dirección: {cliente1.direccion}")
-print(f"Edad: {cliente1.edad}")
+print(f"Nombre: {cliente1.get_nombre()}")
+print(f"Dirección: {cliente1.get_direccion()}")
+print(f"Edad: {cliente1.get_edad()}")
 print("Detalles de la cuenta:")
 cliente1.imprimirDetalles()
 
-
-cuenta1.retirar(100)
-print("Saldo después del retiro: ", cuenta1.saldo)
-cuenta1.depositar(200)
-print("Saldo después del depósito: ", cuenta1.saldo)
-print(cuenta1.saldo)
-
-cuenta1.imprimirDetalles()
+menu1 = Menu("Bienvenido a Banxico", cuenta1)
+menu1.darBienvenida()
+menu1.desplegarOpciones()
 
