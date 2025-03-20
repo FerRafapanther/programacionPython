e4ef4491650cd2e587dbecd97335e6c7cb220f7d
FerRafapanther <fernandorafael@ciencias.unam.mx>
2025-03-20 15:53:37 -0600

    Update Menu.py

--- teoria/relacionesDeClases/Menu.py
+++ teoria/relacionesDeClases/Menu.py
@@ -5,22 +5,39 @@ Created on Tue Feb 18 23:37:01 2025
 @author: Rocket
 """
 # Fecha: Marzo, 2025
-# @version: 1.2
+# @version: 1.3
 # @autor: FernandoRafaelLopezSolano
 
 class Menu:
-    def __init__(self, mensaje):
-        self.mensajeBienvenida= mensaje
+    def __init__(self, mensaje, cuenta):
+        self.__mensajeBienvenida = mensaje
+        self.__cuenta = cuenta
         
     def darBienvenida(self):
-        print(self.mensajeBienvenida)
-        
+        print(self.__mensajeBienvenida)
+
     def OpcionRetirar(self):
         print("Usted seleccionó la opción retirar.")
+        cantidad = float(input('Cantidad que desea retirar:: '))
         
+        if cantidad < 0:
+            print("Error: La cantidad ingresada no puede ser negativa. Intenta nuevamente.")
+        else:
+            mensaje_retiro = self.__cuenta.retirar(cantidad)
+            print(mensaje_retiro)
+            print(f"Saldo después del retiro: {self.__cuenta.get_saldo()}")
+            
     def OpcionDepositar(self):
         print("Usted seleccionó la opción depositar.")
+        cantidad = float(input('Cantidad que desea depositar:: '))
         
+        if cantidad < 0:
+            print("Error: La cantidad ingresada no puede ser negativa. Intenta nuevamente.")
+        else:
+            mensaje_deposito = self.__cuenta.depositar(cantidad)
+            print(mensaje_deposito)
+            print(f"Saldo después del depósito: {self.__cuenta.get_saldo()}") 
+            
     def OpcionSalir(self):
         print("Usted seleccionó la opción salir.")
         
